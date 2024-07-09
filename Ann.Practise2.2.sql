6fe26c2c383c077b6b66f24e11284162fad0e7d9
Annle28082001 <phuonganhlle.work@gmail.com>
2024-07-09 23:03:38 +0100

    Ann.sql to Ann.Practise2.2.sql

--- Ann.Practise2.2.sql
+++ Ann.Practise2.2.sql
@@ -1,3 +1,4 @@
+--part 1: Xuất file có các trường
 With category_data as
 (
 Select 
@@ -25,7 +26,78 @@ round((TPV - Total_cost)/Total_cost,2) as Profit_to_cost_ratio
 from category_data
 Order by Product_category, Year, Month
 
---BAI EM TỰ CODE CŨ
+--PART 2: COHORT RETENTION CHART
+
+WITH online_retail_index as 
+(SELECT 
+id,
+revenue,
+FORMAT_DATE('%Y-%m', created_at) as Cohort_date,
+(FORMAT_DATE('%Y', created_at) - FORMAT_DATE('%Y', first_purchase_date))*12 + 
+(FORMAT_DATE('%M', created_at) - FORMAT_DATE('%M', first_purchase_date)) + 1 as index 
+FROM
+    (SELECT 
+    a.id,
+    SUM(sale_price) OVER (PARTITION BY product_id) AS revenue,
+    MIN(created_at) OVER (PARTITION BY product_id) AS first_purchase_date,
+    b.created_at
+    FROM bigquery-public-data.thelook_ecommerce.products AS a
+    JOIN bigquery-public-data.thelook_ecommerce.order_items AS b
+    ON a.id = b.product_id)
+)
+, XXX AS (SELECT 
+Cohort_date,
+index,
+COUNT(DISTINCT id) as cnt,
+SUM(revenue) as total_revenue
+FROM online_retail_index
+GROUP BY cohort_date, index)
+
+, customer_cohort as 
+(SELECT 
+cohort_date, 
+sum(CASE WHEN index=1 then cnt else 0 end) as m1, 
+sum(CASE WHEN index=2 then cnt else 0 end) as m2,
+sum(CASE WHEN index=3 then cnt else 0 end) as m3,
+sum(CASE WHEN index=4 then cnt else 0 end) as m4,
+sum(CASE WHEN index=5 then cnt else 0 end) as m5,
+sum(CASE WHEN index=6 then cnt else 0 end) as m6,
+sum(CASE WHEN index=7 then cnt else 0 end) as m7,
+sum(CASE WHEN index=8 then cnt else 0 end) as m8,
+sum(CASE WHEN index=9 then cnt else 0 end) as m9,
+sum(CASE WHEN index=10 then cnt else 0 end) as m10,
+sum(CASE WHEN index=11 then cnt else 0 end) as m11,
+sum(CASE WHEN index=12 then cnt else 0 end) as m12,
+sum(CASE WHEN index=13 then cnt else 0 end) as m13
+FROM xxx
+group by cohort_date
+order by cohort_date)
+
+--retention cohort 
+SELECT 
+cohort_date,
+(100- round(100.00* m1/m1,2)) || '%' as m1,
+(100- round(100.00* m2/m1,2)) || '%' as m2,
+(100 -round(100.00* m2/m1,3)) || '%' as m3,
+(100- round(100.00* m2/m1,4)) || '%' as m4,
+(100- round(100.00* m2/m1,5)) || '%' as m5,
+(100- round(100.00* m2/m1,6)) || '%' as m6,
+(100- round(100.00* m2/m1,7)) || '%' as m7,
+(100- round(100.00* m2/m1,8)) || '%' as m8,
+(100- round(100.00* m2/m1,9)) || '%' as m9,
+(100- round(100.00* m2/m1,10)) || '%' as m10,
+(100- round(100.00* m2/m1,11)) || '%' as m11,
+(100- round(100.00* m2/m1,12)) || '%' as m12,
+(100- round(100.00* m2/m1,13)) || '%' as m13
+from customer_cohort
+
+Bị lỗi: 
+/* No matching signature for operator - for argument types: STRING, STRING. Supported signatures: INT64 - INT64; NUMERIC - NUMERIC; BIGNUMERIC - BIGNUMERIC; 
+FLOAT64 - FLOAT64; DATE - INT64; DATE - DATE; TIMESTAMP - TIMESTAMP; DATETIME - DATETIME; TIME - TIME; TIMESTAMP - INTERVAL; DATE - INTERVAL; DATETIME - INTERVAL;
+INTERVAL - INTERVAL at [7:2]
+*/
+
+--BAI EM TỰ CODE CŨ: bị lỗi ở chỗ vẫn bị trùng lặp dữ liệu dù em đã thêm DISTINCT ở MONTH
 
 WITH CTE1 AS (
     SELECT 
